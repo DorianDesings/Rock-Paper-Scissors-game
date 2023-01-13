9b1d7b98cba0b991d04aa69bc4ee1c9850108d89
DorianDesings <dorianoriginaldesings@gmail.com>
2023-01-13 18:51:35 +0100

    game advance fixed

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -6,7 +6,13 @@ const mainElement = document.getElementById('main');
 const firstStepElement = document.getElementById('first-step');
 const secondStepElement = document.getElementById('second-step');
 
+const pickedUserContainer = document.getElementById('picked-user-container');
+const pickedUserImage = document.getElementById('picked-user-image');
+
 const gameResultElement = document.getElementById('game-result');
+
+const pickedPcContainer = document.getElementById('picked-pc-container');
+const pickedPcImage = document.getElementById('picked-pc-image');
 
 const playAgainElement = document.getElementById('play-again');
 
@@ -47,21 +53,17 @@ const GAME_RULES = {
 };
 
 let currentScreen = 1;
+let pcSelection;
+let userSelection;
 let pointsUser = 0;
 let pointsPc = 0;
-let userIcon;
-let pcIcon;
 
 if (GAME_MODE === 'advanced') {
   GAME_OPTIONS.push('lizard', 'spock');
 }
 
-const resetGame = () => {
-  userIcon.classList.remove('game-item--show');
-  pcIcon.classList.remove('game-item--show');
-};
-
 const changeScreen = () => {
+  console.log(pcSelection, userSelection);
   if (currentScreen === 1) {
     currentScreen = 2;
     firstStepElement.classList.remove('first-step--show');
@@ -70,6 +72,14 @@ const changeScreen = () => {
     currentScreen = 1;
     firstStepElement.classList.add('first-step--show');
     secondStepElement.classList.remove('second-step--show');
+    pickedPcContainer.classList.remove(
+      'game-item--show',
+      `game-item--${pcSelection}`
+    );
+    pickedUserContainer.classList.remove(
+      'game-item--show',
+      `game-item--${userSelection}`
+    );
   }
 };
 
@@ -100,28 +110,40 @@ const checkWinner = (userPlay, pcPlay) => {
   updateScore();
 };
 
-const optionsSelection = userSelection => {
-  const pcSelection = selectPcElement();
-  userIcon = document.getElementById(`${userSelection}-user`);
-  pcIcon = document.getElementById(`${pcSelection}-pc`);
-  userIcon.classList.add('game-item--show');
-  pcIcon.classList.add('game-item--show');
+const printResults = (userSelection, pcSelection) => {
+  console.log(userSelection, '---', pcSelection);
+  pickedUserContainer.classList.add(`game-item--${userSelection}`);
+  pickedUserImage.src = `assets/images/icon-${userSelection}.svg`;
+  pickedPcImage.src = `assets/images/icon-${pcSelection}.svg`;
+  pickedPcContainer.classList.add(`game-item--${pcSelection}`);
+
+  const timeoutId = setTimeout(() => {
+    pickedPcContainer.classList.add(`game-item--show`);
+    pickedUserContainer.classList.add(`game-item--show`);
+    clearInterval(timeoutId);
+  }, 100);
+};
+
+const optionsSelection = () => {
+  pcSelection = selectPcElement();
+  printResults(userSelection, pcSelection);
+
   checkWinner(userSelection, pcSelection);
 };
 
 const selectPcElement = () => {
-  const result = Math.floor(Math.random() * (GAME_OPTIONS.length - 1));
+  const result = Math.floor(Math.random() * GAME_OPTIONS.length);
   return GAME_OPTIONS[result];
 };
 
 firstStepElement.addEventListener('click', ev => {
   if (ev.target.classList.contains('game-item__image')) {
     changeScreen();
-    optionsSelection(ev.target.dataset.element);
+    userSelection = ev.target.dataset.element;
+    optionsSelection();
   }
 });
 
 playAgainElement.addEventListener('click', () => {
   changeScreen();
-  resetGame();
 });
